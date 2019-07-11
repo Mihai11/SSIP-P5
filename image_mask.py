93257c6c75cc4a3d896da8037abaa6dd95359936
Mihai SAMSON <msamson@ias.bitdefender.biz>
2019-07-11 18:16:33 +0300

    rotire cu PCA

--- image_mask.py
+++ image_mask.py
@@ -1,9 +1,11 @@
 import cv2
 import numpy as np
+import os
+from math import atan2, cos, sin, sqrt, pi
+from PIL import Image
 
 
-
-def process_image(image_file):
+def bbox_image(image_file):
     minBGR = (250, 250, 250)
     maxBGR = (255, 255, 255)
     with open(image_file, 'rb') as f:
@@ -13,31 +15,82 @@ def process_image(image_file):
         maskBGR = cv2.inRange(image, minBGR, maxBGR)
         sum_vector = maskBGR.sum(axis=0)
 
-        kernel = np.ones((100, 100), np.uint8)
+        kernel = np.ones((60, 60), np.uint8)
         # dilate maskBGR to take over edges of pixels of color
         maskBGR = cv2.erode(maskBGR, kernel, iterations=2)
         # resultLAB = cv2.bitwise_and(original, original, mask=maskBGR)
         bbox = cv2.boundingRect(maskBGR)
         # print(bbox)
         x, y, w, h = bbox
-        if sum(sum_vector[:int(len(sum_vector)/2)]) > sum(sum_vector[int(len(sum_vector)/2):]):
-            x -= int(0.035 * len(maskBGR[0]))
-            w += int(0.035 * len(maskBGR[0]))
-            print("cotor pe stanga")
+        if sum(sum_vector[:int(len(sum_vector) / 2)]) > sum(sum_vector[int(len(sum_vector) / 2):]):
+            x -= int(0.02 * len(maskBGR[0]))
+            w += int(0.02 * len(maskBGR[0]))
         else:
-            w += int(0.035 * len(maskBGR[0]))
-            print("cotor pe dreapta")
-        crop = original[y:y + h, x:x + w]
+            w += int(0.02 * len(maskBGR[0]))
+        return x, y, w, h
 
-        cv2.imwrite(image_file+"_p.png", crop, [int(cv2.IMWRITE_JPEG_QUALITY), 70])
+
+def crop_image(x, y, w, h, image_file):
+    with open(image_file, 'rb') as f:
+        original = cv2.imdecode(np.frombuffer(f.read(), dtype=np.uint8), cv2.IMREAD_COLOR)
+        crop = original[y:y + h, x:x + w]
+        output_file = os.path.dirname(image_file) + "\\processed\\" + image_file.split("\\")[-1]
+        cv2.imwrite(output_file, crop, [int(cv2.IMWRITE_JPEG_QUALITY), 70])
+
+
+def getOrientation(pts, img):
+    sz = len(pts)
+    data_pts = np.empty((sz, 2), dtype=np.float64)
+    for i in range(data_pts.shape[0]):
+        data_pts[i, 0] = pts[i, 0, 0]
+        data_pts[i, 1] = pts[i, 0, 1]
+    # Perform PCA analysis
+    mean = np.empty((0))
+    mean, eigenvectors, eigenvalues = cv2.PCACompute2(data_pts, mean)
+    # Store the center of the object
+    cntr = (int(mean[0, 0]), int(mean[0, 1]))
+
+    cv2.circle(img, cntr, 3, (255, 0, 255), 2)
+    angle = atan2(eigenvectors[0, 1], eigenvectors[0, 0])  # orientation in radians
+
+    return angle, cntr
+
+
+def get_rotation_angle(image_file):
+    image = Image.open(image_file)  # open colour image
+    image = image.convert('L').point(lambda band: 255 if band > 250 else 0)  # convert image to black and white
+    image.save(image_file + "_p.png")
+    src = cv2.imread(image_file + "_p.png")
+    # Convert image to grayscale
+    gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    # Convert image to binary
+    _, bw = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    # Find all the contours in the thresholded image
+    contours, _ = cv2.findContours(bw, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    contour = 0
+    max_area = 0
+    for i, c in enumerate(contours):
+        # Calculate the area of each contour
+        area = cv2.contourArea(c)
+        if area > max_area:
+            max_area, contour = area, c
+        # # Draw each contour only for visualisation purposes
+        # cv2.drawContours(src, contours, i, (0, 0, 255), 2)
+        # Find the orientation of each shape
+    rect = cv2.minAreaRect(contour)
+    angle_rect = rect[2]
+    if angle_rect < -50:
+        angle_rect += 90
+    angle, cntr = getOrientation(contour, src)
+    print("rectangular: ", angle_rect, "angle", angle)
+    # cv2.drawContours(src, contours, contours.index(contour), (0, 0, 255), 2)
+    # rot_mat = cv2.getRotationMatrix2D(cntr, -angle, 1.0)
+    rot_mat = cv2.getRotationMatrix2D(cntr, -angle, 1.0)
+    return cv2.warpAffine(cv2.imread(image_file), rot_mat, src.shape[1::-1], flags=cv2.INTER_LINEAR)
 
 
 if __name__ == '__main__':
     image_file = 'Data/-374-rau/-374-rau_out_2.png'
-
-
-
-
 
 """
 def recognize_remove_mark(doc, image_classif, mark_removed_name=None,
--- main.py
+++ main.py
@@ -3,12 +3,9 @@ from pdf2image import convert_from_path
 from scipy import misc
 from PIL import Image
 import numpy as np
-from resizeimage import resizeimage
 import matplotlib.pyplot as plt
 import os
-from copy import copy
-from image_mask import process_image
-
+from image_mask import *
 
 do_pdf2img = False
 do_color2bw = False
@@ -20,7 +17,7 @@ if do_pdf2img:
         if os.path.isfile(os.path.join("Data", file_name)):
             file_path = os.path.join("Data/", file_name)
 
-            pages = convert_from_path(file_path, 500)
+            pages = convert_from_path(file_path, 300)
             for index, page in enumerate(pages):
                 picture_path = '{}/{}_out_{}.png'.format(file_path.split(".")[0], file_name.split(".")[0], index)
                 page.save(picture_path, 'PNG')
@@ -33,60 +30,8 @@ if do_color2bw:
             for file in os.listdir(os.path.join("Data", dir)):
                 file_path = os.path.join(os.path.join("Data", dir), file)
                 image_file = Image.open(file_path)  # open colour image
-                image_file = image_file.convert('L').point(
-                    lambda band: 255 if band > 240 else 0)  # convert image to black and white
+                image_file = image_file.convert('L').point(lambda band: 255 if band > 240 else 0)  # convert image to black and white
                 image_file.save(file_path)
-
-
-def matprint(mat):
-    fd = open("exp.txt", "w")
-    for x in mat:
-        for i, y in enumerate(x):
-            y = 1 if y > 0 else 0
-            fd.write(str(y))
-        fd.write("\n")
-    fd.close()
-
-
-def BFS(matrix):
-    matrix_copy = copy(matrix)
-    Xs = [0, len(matrix[0]) - 1]
-    Ys = [0, len(matrix) - 1]
-    for x in Xs:
-        for y in Ys:
-            queue = list()
-            queue.append((y, x))
-            index = 0
-            while index < len(queue):
-                y, x = queue[index]
-                if matrix[y][x] == 0:
-                    matrix[y][x] = 255
-                    matrix_copy[y][x] = -1
-                    if y > 0 and matrix_copy[y - 1][x] != -1:
-                        queue.append((y - 1, x))
-                    if y > 0 and x > 0 and matrix_copy[y - 1][x - 1] != -1:
-                        queue.append((y - 1, x - 1))
-                    if x > 0 and matrix_copy[y][x - 1] != -1:
-                        queue.append((y, x - 1))
-                    if y < Ys[1] and x > 0 and matrix_copy[y + 1][x - 1] != -1:
-                        queue.append((y + 1, x - 1))
-                    if y < Ys[1] and matrix_copy[y + 1][x] != -1:
-                        queue.append((y + 1, x))
-                    if y < Ys[1] and x < Xs[1] and matrix_copy[y + 1][x + 1] != -1:
-                        queue.append((y + 1, x + 1))
-                    if x < Xs[1] and matrix_copy[y][x + 1] != -1:
-                        queue.append((y, x + 1))
-                    if y > 0 and x < Xs[1] and matrix_copy[y - 1][x + 1] != -1:
-                        queue.append((y - 1, x + 1))
-                index += 1
-    return matrix
-
-
-# resize a picture
-# with open('test-image.jpeg', 'r+b') as f:
-#     with Image.open(f) as image:
-#         cover = resizeimage.resize_cover(image, [200, 100])
-#         cover.save('test-image-cover.jpeg', image.format)
 
 # convert black and white images to matrices
 if do_process_image:
@@ -94,12 +39,16 @@ if do_process_image:
         if os.path.isdir(os.path.join("Data", dir)):
             for file in tqdm.tqdm(os.listdir(os.path.join("Data", dir)), desc=dir):
                 file_path = os.path.join(os.path.join("Data", dir), file)
-                process_image(file_path)
-                # matrix = misc.imread(file_path)
-                # sum_vect = np.sum(matrix, axis=1)# / len(matrix[0])
-                # a = np.hstack((matrix.normal(size=1000),matrix.normal(loc=5, scale=2, size=1000)))
-                # plt.hist(np.array(matrix).flatten(), bins='auto')  # arguments are passed to np.histogram
-                # plt.show()
-                # matrix = BFS(matrix)
-                # misc.imsave(file_path, matrix)
-                # matprint(matrix)
+                if os.path.isfile(file_path):
+                    rotated_image = get_rotation_angle(file_path)
+                    misc.imsave(file_path + "_p.png", rotated_image)
+                    x, y, w, h = bbox_image(file_path + "_p.png")
+                    crop_image(x, y, w, h, file_path)
+                    # matrix = misc.imread(file_path)
+                    # sum_vect = np.sum(matrix, axis=1)# / len(matrix[0])
+                    # a = np.hstack((matrix.normal(size=1000),matrix.normal(loc=5, scale=2, size=1000)))
+                    # plt.hist(np.array(matrix).flatten(), bins='auto')  # arguments are passed to np.histogram
+                    # plt.show()
+                    # matrix = BFS(matrix)
+                    # misc.imsave(file_path, matrix)
+                    # matprint(matrix)
