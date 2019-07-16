4131104a2dbc114bbaf7e1ecfcd7788db7059457
Gabriel Trautmann <gabriel.trautmann@ness.com>
2019-07-16 08:14:22 +0300

    nn for autoorientation

--- nn_orientation_trainer.py
+++ nn_orientation_trainer.py
@@ -1,3 +1,6 @@
+import cv2
+import numpy as np
+
 from nn_orientation import ImageOrientationSequence, setup_model
 
 
@@ -5,8 +8,22 @@ def train(training_folder, validation_folder):
     train_generator = ImageOrientationSequence(training_folder, batches_per_iteration=100)
     validation_generator = ImageOrientationSequence(validation_folder, batches_per_iteration=10)
     mX, my = train_generator[0]
-    model = setup_model(mX,my)
-    model.fit_generator(train_generator,epochs=3,validation_data=validation_generator)
+    model = setup_model(mX, my)
+    model.fit_generator(train_generator, epochs=3, validation_data=validation_generator)
+    return model
+
+
+def get_orientation(model, image):
+    if len(image.shape) == 2:
+        image = image.reshape((image.shape + (1,)))
+    else:
+        if image.shape[-1] != 1:
+            raise Exception('Expected to have grayscale image')
+    if np.max(image) > 1.:
+        raise Exception('Image is expected to be normalized to 1')
+    result = model.predict(image)
+    print(f'orientation_result shape {result.shape}')
+    return np.argmax(result[0,:])
 
 
 def generator_test(input_folder):
@@ -18,4 +35,8 @@ def generator_test(input_folder):
 
 if __name__ == '__main__':
     # generator_test('orientation_data/train')
-    train('orientation_data/train', 'orientation_data/validation')
+    model = train('orientation_data/train', 'orientation_data/validation')
+    img = cv2.imread('orientation_data/validation/g_005.png', cv2.IMREAD_GRAYSCALE)
+    img = img / 255
+    result = get_orientation(model, img)
+    print(result)
